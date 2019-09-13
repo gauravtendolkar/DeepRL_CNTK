30134976dcc75370278b88ccf2764abd6952b26f
Ubuntu <gatendol@cntk-rl.zehy5ew13ygubot15radupssbd.bx.internal.cloudapp.net>
2019-09-13 17:50:44 +0000

    python virtual display; dqn ram agent demo video

--- core/agents/dqn/evaluate.py
+++ core/agents/dqn/evaluate.py
@@ -1,11 +1,12 @@
 import gym
 import os
-from agents.dqn.dqn import Agent
+from agents.dqn.dqn import RAMAgent
 from utils.preprocessing import downscale
+import numpy as np
 
 # Create environment
 ENV_NAME = 'LunarLander-v2'
-env = gym.make(ENV_NAME, frameskip=2)
+env = gym.make(ENV_NAME)
 
 NUM_STATES = env.observation_space.shape
 
@@ -13,16 +14,14 @@ NUM_ACTION_VALUES = env.action_space.n  # env.action_space.n
 
 base_path = os.path.dirname(os.path.abspath(__file__))
 
-agent = Agent(num_actions=NUM_ACTION_VALUES, observation_space_shape=(84, 84), pretrained_policy=base_path+'/pretrained/LunarLander.model', explore=False)
+agent = RAMAgent(num_actions=NUM_ACTION_VALUES, observation_space_shape=NUM_STATES, pretrained_policy=base_path+'/pretrained/LunarLander.model', explore=False)
 
 
 # Create a function that runs ONE episode and returns cumulative reward at the end
 def run(render=False):
     # Reset the environment to get the initial state. current_state is a single RGB [210 x 160 x 3] image
     current_state = env.reset()
-    current_state = downscale(current_state)
-    current_state = agent.evaluation_policy.frame_stacker.add_frame(current_state)
-
+    current_state = np.array(current_state, dtype=np.float32)
     # Set cumulative episode reward to 0
     cumulative_reward = 0
 
@@ -31,12 +30,11 @@ def run(render=False):
         # Based of agent's exploration/exploitation policy, either choose a random action or do a
         # forward pass through agent's policy to obtain action
         current_action = agent.act(current_state)
-        action_to_take = current_action + 1
+        action_to_take = current_action
 
         # Take a step in environment
         next_state, reward, is_done, info = env.step(action_to_take)
-        next_state = downscale(next_state)
-        next_state = agent.evaluation_policy.frame_stacker.add_frame(next_state)
+        next_state = np.array(next_state, dtype=np.float32)
 
         current_state = next_state
 
@@ -51,14 +49,16 @@ def run(render=False):
         # Note that the saving part is the only CNTK specific code in this entire file
         # Ensuring such modularities are key to building complex libraries
         if is_done:
-            agent.evaluation_policy.frame_stacker.reset()
-            agent.memory.reset()
             return cumulative_reward
 
 
 print("Training Starts..")
 
 # Training code
+from pyvirtualdisplay import Display
+virtual_display = Display(visible=0, size=(1400, 900))
+virtual_display.start()
+
 vr = gym.wrappers.monitoring.video_recorder.VideoRecorder(env, base_path+'/pretrained/demo.mp4')
 episode_reward = run(render=False)
 vr.close()
